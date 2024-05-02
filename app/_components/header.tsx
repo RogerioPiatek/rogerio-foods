7481014615d23eaf1fc7b99dd6d53d8519869ccd
Rogerio <rogeriopiatek@gmail.com>
2024-05-01 21:51:35 -0300

    style: improve logo quality on header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,14 +1,26 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="flex justify-between px-5 pt-6">
-      <div className="flex gap-1">
-        <h1 className="text-lg font-bold uppercase text-[#EA1D2C]">Rogério</h1>
-        <Image src="/logo.png" alt="Rogério Foods" height={50} width={60} />
-      </div>
+      <Link href="/">
+        <div className="flex gap-2">
+          <h1 className="text-lg font-bold uppercase italic text-[#EA1D2C]">
+            Rogério
+          </h1>
+          <div className="relative h-[30px] w-[50px]">
+            <Image
+              src="/logo.png"
+              alt="Rogério Foods"
+              fill
+              className="object-cover"
+            />
+          </div>
+        </div>
+      </Link>
       <Button
         size="icon"
         variant="outline"
